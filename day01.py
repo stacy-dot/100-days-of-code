a0ec1d3cd21261f1054a093217453c7850d6d4f5
stacy-dot <133856234+stacy-dot@users.noreply.github.com>
2025-06-06 13:08:13 +0300

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,14 @@
+print("Hello world, i'm Stacy and this day 1 of #100daysofcode")
+#working with variables
+name=input("Enter your name")
+age=input("Enter your age")
+print(f"Nice to meet you {name}. You are {age} years old")
+
+#checking data types
+print("'name' is a:", type(name))
+print("'age' is a:", type(age))
+
+#typecasting
+Age=int(age)+5
+
+print(f"{name}, you'll be {Age} in 5 years time.")
