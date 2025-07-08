e547653d82d8f44a14c6a7e3c96da121c06312bc
Stacy Karara <133856234+stacy-dot@users.noreply.github.com>
2025-07-08 19:39:31 +0300

    Add files via upload

--- day11.py
+++ day11.py
@@ -1,40 +1,100 @@
-import random
-import string
+'''import matplotlib.pyplot as plt
 
-def generate_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return password
+expenses=[
+    {"item": "Rent", "amount": 15000},
+    {"item": "food", "amount":6000},
+    {"item": "Transport", "amount":5000},
+    {"item": "internet", "amount":2000}
+    
+]
 
-def check_strength(password):
-    has_letters = any(char.isalpha() for char in password)
-    has_digits = any(char.isdigit() for char in password)
-    has_symbols = any(char in string.punctuation for char in password)
-    length = len(password)
+def show_barchart():
+    items=[expense["item"] for expense in expenses]
+    amount=[expense["amount"] for expense in expenses]
 
-    if length < 6:
-        return "Weak"
-    elif length <= 10 and has_letters and has_digits:
-        return "Moderate"
-    elif length > 10 and has_letters and has_digits and has_symbols:
-        return "Strong"
-    else:
-        return "Weak"
+    plt.figure (figsize=(8,5))
+    plt.bar(items,amount,color='skyblue')
+    plt.title("Monthly Expenses")
+    plt.xlabel("Category")
+    plt.ylabel("Amount(KSH)")
+    plt.tight_layout()
+    plt.show()
 
+def show_pie_chart():
+    items = [expense["item"] for expense in expenses]
+    amounts = [expense["amount"] for expense in expenses]
+
+    plt.figure(figsize=(6, 6))
+    plt.pie(amounts, labels=items, autopct='%1.1f%%', startangle=140)
+    plt.title("Expense Distribution")
+    plt.tight_layout()
+    plt.show()
+
+
+show_barchart()
+show_pie_chart()'''
+
+import matplotlib.pyplot as plt
+
+expenses=[]
+
+def add_expense():
+    while True:
+        item=input("Enter item name(or done to finish):")
+        if item.lower()=="done":
+            break
+        try:
+            amount=float(input(f"Amount spent on {item}:"))
+            expenses.append({"item":item,"amount":amount})
+        except ValueError:
+            print("Invalid amount.Try again!")
+
+def show_barchart():
+    if not expenses:
+        print("No data to display. Add expenses first.")
+        return
+    amount=[expense["amount"] for expense in expenses]
+    item=[expense["item"] for expense in expenses]
+    total=sum(expense["amount"] for expense in expenses)
+
+
+    plt.figure(figsize=(7,4))
+    plt.bar(item, amount,color=['#FF6F61', '#6B5B95', '#88B04B', '#F7CAC9'])
+    plt.title(f"Monthly Expeses-Total:ksh{total}")
+    plt.xlabel("item")
+    plt.ylabel("amount")
+    plt.tight_layout()
+    plt.show()
+
+def show_piechart():
+    if not expenses:
+        print("No data to display.Add expenses first!")
+    item=[expense["item"] for expense in expenses]
+    amount=[expense["amount"] for expense in expenses]
+    total=sum(amount)
+
+    plt.figure(figsize=(5,5))
+    plt.pie(amount,labels=item,autopct='%1.1f%%' ,startangle=140)
+    plt.title(f"Monthly Expenditure-Total:KSH{total}")
+    plt.tight_layout()
+    plt.show()
 
 while True:
-    print("\n Password Generator with Strength Meter")
-    try:
-        length = int(input("Enter password length (or 0 to exit): "))
-        if length == 0:
-            print("Goodbye!")
-            break
-        elif length < 4:
-            print("Too short! Must be at least 4 characters.")
-        else:
-            pwd = generate_password(length)
-            strength = check_strength(pwd)
-            print(f"\nGenerated Password: {pwd}")
-            print(f" Strength: {strength}")
-    except ValueError:
-        print("Please enter a valid number.")
+    print("---Expense menu---")
+    print("1.Add expenses")
+    print("2.Show bar chart")
+    print("3.Show pie chart")
+    print("4.exit")
+    choice=input("Choose option 1-4:")
+
+    if choice=="1":
+        add_expense()
+    elif choice=="2":
+        show_barchart()
+    elif choice=="3":
+        show_piechart()
+    elif choice=="4":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid option!Try again.")
